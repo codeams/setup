08cf59a970a760e6c0f4282491083ab04d05b9a7
Erick A. Montañez <codeams@gmail.com>
2017-06-21 22:01:08 -0500

    Add sublime settings

---
+++ configuration-files/Preferences.sublime-settings.js
@@ -0,0 +1,19 @@
+{
+  "bold_folder_labels": true,
+  "caret_style": "smooth",
+  "color_scheme": "Packages/Material Theme/schemes/Material-Theme-Palenight.tmTheme",
+  "draw_indent_guides": false,
+  "font_face": "Operator Mono", // was: Monaco
+  "font_size": 15, // was: 15
+  "ignored_packages":
+  [
+    "Vintage"
+  ],
+  "line_padding_bottom": 10,
+  "line_padding_top": 10,
+  "material_theme_accent_blue": true,
+  "material_theme_small_tab": true,
+  "tab_size": 2,
+  "theme": "Material-Theme-Palenight.sublime-theme",
+  "translate_tabs_to_spaces": true
+}
