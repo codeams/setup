bd598ae30f9fe42957419dc07cd65351152d1f8d
Erick A. Montañez <codeams@gmail.com>
2017-07-04 12:10:04 -0500

    Format hyper configuration file

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -1,123 +1,53 @@
-// Future versions of Hyper may add additional config options,
-// which will not automatically be merged into this file.
-// See https://hyper.is#cfg for all currently supported options.
-
 module.exports = {
   config: {
-    // default font size in pixels for all tabs
+    // Font configuration
     fontSize: 17,
-
-    // font family with optional fallbacks
     fontFamily: '"Operator Mono", Monaco, monospace',
 
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    // cursorColor: '#F8C972',
-
-    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
+    // Cursor configuration
     cursorShape: 'UNDERLINE',
-
-    // set to true for blinking cursor
     cursorBlink: true,
 
-    // color of the text
+    // Text coloring configuration
     // foregroundColor: '#fff',
-
-    // terminal background color
     // backgroundColor: '#000',
 
-    // border color (window, tabs)
-    // borderColor: '#333',
-
-    // custom css to embed in the main window
+    // CSS configuration
     // css: '',
-
-    // custom css to embed in the terminal window
     termCSS: `
       x-screen {
         -webkit-font-smoothing: antialiased !important;
       }
     `,
 
-    // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
-    // default: `false` on Linux, `true` on Windows (ignored on macOS)
-    // showHamburgerMenu: '',
+    // Window configuration
+    padding: '24px 24px',
+    colors: {},
 
-    // set to `false` if you want to hide the minimize, maximize and close buttons
-    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
-    // default: `true` on windows and Linux (ignored on macOS)
-    // showWindowControls: '',
-
-    // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
-
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
-    colors: {
-      // black: '#000000',
-      // red: '#ff0000',
-      // green: '#33ff00',
-      // yellow: '#ffff00',
-      // blue: '#0066ff',
-      // magenta: '#cc00ff',
-      // cyan: '#00ffff',
-      // white: '#d0d0d0',
-      // lightBlack: '#808080',
-      // lightRed: '#ff0000',
-      // lightGreen: '#33ff00',
-      // lightYellow: '#ffff00',
-      // lightBlue: '#0066ff',
-      // lightMagenta: '#cc00ff',
-      // lightCyan: '#00ffff',
-      // lightWhite: '#ffffff'
-    },
-
+    // Material theme configuration
     MaterialTheme: {
-        // Set the theme variant,
-        // OPTIONS: 'Darker', 'Palenight', ''
-        theme: 'Palenight',
+      // OPTIONS: 'Darker', 'Palenight', ''
+      theme: 'Palenight',
 
-        // Set the rgba() app background opacity, useful when enableVibrance is true
-        // OPTIONS: From 0.1 to 1
-        backgroundOpacity: '0.7',
+      backgroundOpacity: '0.7',
+      accentColor: '#64FFDA',
 
-        // Set the accent color for the current active tab
-        accentColor: '#64FFDA',
-
-        // Mac Only. Need restart. Enable the vibrance and blurred background
-        // OPTIONS: 'dark', 'ultra-dark', 'bright'
-        // NOTE: The backgroundOpacity should be between 0.1 and 0.9 to see the effect.
-        vibrancy: 'ultra-dark'
+      // Need restart. Enable the vibrance and blurred background
+      // OPTIONS: 'dark', 'ultra-dark', 'bright'
+      // NOTE: The backgroundOpacity should be between 0.1 and 0.9 to see the effect.
+      vibrancy: 'ultra-dark'
     },
 
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
-    // make sure to use a full path if the binary name doesn't work
-    // (e.g `C:\\Windows\\System32\\bash.exe` instead of just `bash.exe`)
-    // if you're using powershell, make sure to remove the `--login` below
+    // Shell configuration
     shell: '/bin/zsh',
-
-    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
-    // by default ['--login'] will be used
-    // shellArgs: ['--login'],
-
-    // for environment variables
+    // shellArgs: ['--login'], // by default ['--login'] will be used
     env: {},
 
-    // set to false for no bell
-    bell: 'SOUND',
-
-    // if true, selected text will automatically be copied to the clipboard
-    copyOnSelect: false
-
-    // if true, on right click selected text will be copied or pasted if no
-    // selection is present (true by default on Windows)
-    // quickEdit: true
-
-    // URL to custom bell
+    // Bahaviour
     // bellSoundURL: 'http://example.com/bell.mp3',
-
-    // for advanced config flags please refer to https://hyper.is/#cfg
+    bell: 'SOUND', // Set to false for no bell
+    copyOnSelect: false
+    // quickEdit: true // if true, on right click selected text will be copied or pasted if no
   },
 
   // a list of plugins to fetch and install from npm
@@ -126,7 +56,9 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyper-material-theme'],
+  plugins: [
+    'hyper-material-theme'
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
