118f93ad35bd29206d68b2dee87a2f92b8e212a5
Erick A. Montañez <codeams@gmail.com>
2018-06-16 12:11:30 -0500

    Update sublime preferences file

--- sublime/Preferences.sublime-settings.js
+++ sublime/Preferences.sublime-settings.js
@@ -5,23 +5,25 @@
 	"color_scheme": "Packages/Material Theme/schemes/Material-Theme-Palenight.tmTheme",
 	"draw_indent_guides": false,
 	"draw_white_space": "all",
-	"font_face": "Operator Mono",
-	"font_size": 15,
-	"use_simple_full_screen": true,
-	"ignored_packages": [
+	"font_face": "Operator Mono Lig",
+	"font_options": [
+		"gray_antialias",
+		"no_round"
 	],
+	"font_size": 13,
+	"ignored_packages": [],
 	"indent_to_bracket": false,
-	"line_numbers": false,
-	"line_padding_bottom": 10,
-	"line_padding_top": 10,
+	"line_numbers": true,
+	"line_padding_bottom": 7,
+	"line_padding_top": 7,
 	"margin": 0,
 	"material_theme_accent_blue": true,
 	"material_theme_small_tab": true,
-	"rulers":
-	[
-	],
+	"rulers": [],
 	"show_tab_close_buttons": true,
 	"tab_size": 2,
 	"theme": "Material-Theme-Palenight.sublime-theme",
-	"translate_tabs_to_spaces": true
+	"translate_tabs_to_spaces": true,
+	"use_simple_full_screen": false,
+	"vintage_start_in_command_mode": true
 }
