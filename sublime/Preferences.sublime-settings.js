837e1c80e994018d899622e359623a7de15d5d51
Erick A. Montañez <codeams@gmail.com>
2018-11-16 12:55:59 -0600

    Update sublime configuration

--- sublime/Preferences.sublime-settings.js
+++ sublime/Preferences.sublime-settings.js
@@ -6,20 +6,25 @@
 	"draw_indent_guides": false,
 	"draw_white_space": "all",
 	"font_face": "Operator Mono Lig",
-	"font_options": [
+	"font_options":
+	[
 		"gray_antialias",
 		"no_round"
 	],
-	"font_size": 13,
-	"ignored_packages": [],
+	"font_size": 15,
+	"ignored_packages":
+	[
+	],
 	"indent_to_bracket": false,
-	"line_numbers": true,
+	"line_numbers": false,
 	"line_padding_bottom": 7,
 	"line_padding_top": 7,
 	"margin": 0,
 	"material_theme_accent_blue": true,
 	"material_theme_small_tab": true,
-	"rulers": [],
+	"rulers":
+	[
+	],
 	"show_tab_close_buttons": true,
 	"tab_size": 2,
 	"theme": "Material-Theme-Palenight.sublime-theme",
@@ -27,3 +32,4 @@
 	"use_simple_full_screen": false,
 	"vintage_start_in_command_mode": true
 }
+
